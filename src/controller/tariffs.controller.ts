4d8f3901f21c93fff05700cfa0feef128ab39123
Elijah <elijah.khrebtov@gmail.com>
2021-05-23 20:02:41 +0300

    fix get tariffs

--- src/controller/tariffs.controller.ts
+++ src/controller/tariffs.controller.ts
@@ -3,7 +3,7 @@ import { rejectionHandler } from '@/middleware/rejection-handler.middleware';
 import { ErrorResponse } from '@/utils/error-response.util';
 
 export const getTarrifs = rejectionHandler(async (req, res) => {
-    const tariffs = await Tariffs.find().limit(1);
+    const [tariffs] = await Tariffs.find().limit(1);
 
     return res.status(200).json(tariffs);
 });
